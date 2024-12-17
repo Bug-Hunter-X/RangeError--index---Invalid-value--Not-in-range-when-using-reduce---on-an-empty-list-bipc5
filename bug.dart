e246c79370c0c1a2aaeb7990fd28e73705b48457
BugHunter-X <askzenai@gmail.com>
2024-12-18 00:14:07 +0100

    Add bug.dart file

---
+++ bug.dart
@@ -0,0 +1,11 @@
+```dart
+List<int> numbers = [1, 2, 3, 4, 5];
+int sum = numbers.reduce((a, b) => a + b);
+print(sum); // Output: 15
+
+// This code will throw an error if the list is empty:
+List<int> emptyNumbers = [];
+int emptySum = emptyNumbers.reduce((a, b) => a + b);
+print(emptySum); // Unhandled exception:
+// RangeError (index): Invalid value: Not in range
+```
